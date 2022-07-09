a70d84f2b3b688c37cca617253f75135ef028602
Elizabeth <102306130+elzbthxo@users.noreply.github.com>
2022-07-08 23:09:22 -0400

    update search api

--- weather.js
+++ weather.js
@@ -1,13 +1,3 @@
-// search city input will change h1
-function citySearch(event) {
-	event.preventDefault();
-	let cityInput = document.querySelector("#cityInput");
-	let cityName = document.querySelector(".cityName");
-	cityName.innerHTML = `${cityInput.value}`;
-}
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", citySearch);
-
 // current day and time will update
 let now = new Date();
 let week = [
@@ -53,3 +43,22 @@ currentDay.innerHTML = `${weekDay}, ${month} ${date}`;
 
 let currentTime = document.querySelector(".currentTime");
 currentTime.innerHTML = `${hours}:${minutes}`;
+
+// search city input will change h1
+function citySearch(event) {
+	event.preventDefault();
+	let cityInput = document.querySelector("#cityInput");
+	let cityName = document.querySelector(".cityName");
+	cityName.innerHTML = `${cityInput.value}`;
+	let city = `${cityInput.value}`;
+	let apiKey = "e8f1caf1080f26b2667bd09ad9d42c74";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q${city}=units=imperial&appid=${apiKey}`;
+	axios.get(apiUrl).then(showWeather);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", citySearch);
+
+// get location input api
+function showWeather(response) {
+	console.log(response);
+}
